c73c28538458ba4bf4ca98333e0995845236a369
CRESPY Arthur <arthur@crespy.eu>
2023-06-22 19:04:36 +0200

    Add ex05

---
+++ ex05/Harl.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 15:09:31 by acrespy           #+#    #+#             */
+/*   Updated: 2023/06/14 15:59:54 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void Harl::debug()
+{
+	std::cout << "\"I love having extra bacon for my"
+				 "7XL-double-cheese-triple-pickle-special-ketchup burger."
+				 "I really do!\"" << std::endl;
+}
+
+void Harl::info()
+{
+	std::cout << "\"I cannot believe adding extra bacon costs more money."
+				 "You didn’t put enough bacon in my burger!"
+				 "If you did, I wouldn’t be asking for more!\"" << std::endl;
+}
+
+void Harl::warning()
+{
+	std::cout << "\"I think I deserve to have some extra bacon for free."
+				 "I’ve been coming for years whereas you started working here since last month.\"" << std::endl;
+}
+
+void Harl::error()
+{
+	std::cout << "\"This is unacceptable! I want to speak to the manager now.\"" << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+	int         i;
+	void		(Harl::*ptr_complain[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	i = 0;
+	while (i < 4)
+	{
+		if (levels[i] == level)
+		{
+			std::cout << "- Harl doing " << level << ": ";
+			(this->*ptr_complain[i])();
+			break ;
+		}
+		i++;
+	}
+	if (i == 4)
+		std::cout << "- Harl doing " << level << ": "
+		<< "\"I don't know what you mean by " << level
+		<< ", but I'm sure it's not important.\"" << std::endl;
+}
---
+++ ex05/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 15:09:31 by acrespy           #+#    #+#             */
+/*   Updated: 2023/06/14 15:59:54 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("HELLO");
+
+    return (0);
+}
+
