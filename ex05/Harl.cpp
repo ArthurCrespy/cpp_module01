ccae5757b79648d555a996cdedb5b4d142529ae0
CRESPY Arthur <arthur@crespy.eu>
2023-06-23 16:26:45 +0200

    Modify printed meessages

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -18,26 +18,30 @@ Harl::~Harl() {}
 
 void Harl::debug()
 {
-	std::cout << "\"I love having extra bacon for my"
-				 "7XL-double-cheese-triple-pickle-special-ketchup burger."
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "\"I love having extra bacon for my "
+				 "7XL-double-cheese-triple-pickle-special-ketchup burger. "
 				 "I really do!\"" << std::endl;
 }
 
 void Harl::info()
 {
-	std::cout << "\"I cannot believe adding extra bacon costs more money."
-				 "You didn’t put enough bacon in my burger!"
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "\"I cannot believe adding extra bacon costs more money. "
+				 "You didn’t put enough bacon in my burger! "
 				 "If you did, I wouldn’t be asking for more!\"" << std::endl;
 }
 
 void Harl::warning()
 {
-	std::cout << "\"I think I deserve to have some extra bacon for free."
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "\"I think I deserve to have some extra bacon for free. "
 				 "I’ve been coming for years whereas you started working here since last month.\"" << std::endl;
 }
 
 void Harl::error()
 {
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "\"This is unacceptable! I want to speak to the manager now.\"" << std::endl;
 }
 
@@ -52,14 +56,15 @@ void Harl::complain(std::string level)
 	{
 		if (levels[i] == level)
 		{
-			std::cout << "- Harl doing " << level << ": ";
 			(this->*ptr_complain[i])();
 			break ;
 		}
 		i++;
 	}
 	if (i == 4)
-		std::cout << "- Harl doing " << level << ": "
-		<< "\"I don't know what you mean by " << level
-		<< ", but I'm sure it's not important.\"" << std::endl;
+	{
+		std::cout << "[ " << level << " ]" << std::endl;
+		std::cout << "Probably complaining about insignificant problems..." << std::endl;
+	}
+
 }
